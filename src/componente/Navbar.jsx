cb054f297fdec7937ce6ea36b3413ca7ed91213d
JESUS EUGENIO RICARDEZ <jericrdez.c03@ucamp.utel.edu.mx>
2022-01-12 22:23:33 -0600

    Update Navbar.jsx
    Cambios en NavBar para ajustar posicion del Search

--- src/componente/Navbar.jsx
+++ src/componente/Navbar.jsx
@@ -6,23 +6,20 @@ export const Navbar = () => {
 
    function busqueda() {
       setUsuario(user.current.value);
-      console.log(usuario);
+      
    }
 
    return (
       <div>
        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <div className="container-fluid">
-    <p className="navbar-brand" href="#">Trabajo Grupal REACT-HOOKS</p>
-    
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <form className="d-flex">
-        <input useRef={user}  className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-        <button className="btn btn-outline-success" onClick={busqueda}>Search</button>
-      </form>
-    </div>
-  </div>
-</nav>  
+         <div className="container-fluid">
+            <p className="navbar-brand" href="#">Trabajo Grupal REACT-HOOKS</p>
+            <form className="d-flex">
+               <input ref={user}  className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+               <button className="btn btn-outline-success" onClick={busqueda}>Search</button>
+            </form>
+         </div>
+        </nav>  
       </div>
    )
 }
